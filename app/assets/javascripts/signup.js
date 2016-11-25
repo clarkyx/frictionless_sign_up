8b43547f76ac14550109925c0e19b007469d3cd1
clark chen <chenhuajie1@gmail.com>
2016-11-24 23:42:52 -0500

    add company role

--- app/assets/javascripts/signup.js
+++ app/assets/javascripts/signup.js
@@ -13,13 +13,15 @@ $(function(){
       method: "post",
       dataType: 'json'
     }).done(function(response){
+      console.log(response);
       if(!response){
         $('#information').trigger('reset');
       }else{
         $('#emailresult').val(response.person.email);
         $('#firstname').val(response.person.name.givenName);
         $('#lastname').val(response.person.name.familyName);
-        $('#companyname').val(response.company);
+        $('#role').val(response.person.employment.title);
+        $('#companyname').val(response.person.employment.name);
       };
     });
   });
