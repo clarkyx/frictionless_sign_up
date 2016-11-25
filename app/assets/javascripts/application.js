77c818873f71502c6f854461b46b3f138a031811
clark chen <chenhuajie1@gmail.com>
2016-11-25 03:35:49 -0500

    add cool looking alert for no information related email

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,8 @@
 //= require jquery
 //= require jquery.turbolinks
 //= require jquery_ujs
+//= require jAlert
+//= require jAlert-functions
 //= require foundation
 //= require turbolinks
 //= require_tree .
